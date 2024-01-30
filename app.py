4e18e214fb85a575ad41935d690f4974669cfcc5
Carrizales Aguilar <MX20K20304@akad.kaercher.com>
2024-01-30 09:41:21 -0600

    connection to the database

---
+++ app.py
@@ -0,0 +1,26 @@
+import pyodbc
+
+SERVER = 'MX20-03093\\SQLTEST'
+DATABASE = 'warehouse'
+USERNAME = 'sa'
+PASSWORD = '*******'
+
+print(SERVER)
+connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+
+conn = pyodbc.connect(connectionString)
+
+SQL_QUERY = """ 
+SELECT TOP (1000) [Product_Id]
+      ,[Name]
+      ,[Price]
+      ,[Description]
+  FROM [warehouse].[dbo].[Product] 
+  """
+
+cursor = conn.cursor()
+cursor.execute(SQL_QUERY)
+
+records = cursor.fetchall()
+for r in records: 
+    print(f"{r.Product_Id}\t{r.Name}\t{r.Price}\t{r.Description}")
